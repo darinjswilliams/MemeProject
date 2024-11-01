f81290881618614ab8dcef925b3500f2bb075b13
darinwilliams <darinjswilliams@icloud.com>
2024-11-01 16:53:41 -0500

    fix pycode style

--- src/CustomException/__init__.py
+++ src/CustomException/__init__.py
@@ -4,4 +4,3 @@ from .ModelException import ModelException
 from .NoDirectoryException import NoDirectoryException
 from .ParseImportException import ParseImportException
 from .RequireParamException import RequireParamException
-
